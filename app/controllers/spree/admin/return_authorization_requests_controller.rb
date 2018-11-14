7080a095bb904ebbe871bea99bec30017adfbddd
Noel <noel@2bedigital.com>
2018-11-14 17:25:38 +0000

    improvements in show return__athorizations

--- app/controllers/spree/admin/return_authorization_requests_controller.rb
+++ app/controllers/spree/admin/return_authorization_requests_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class ReturnAuthorizationRequestsController < BaseController
       def index
-        @return_authorizations = Spree::ReturnAuthorization.where(state: 'pending')
+        @return_authorizations = Spree::ReturnAuthorization.all
       end
     end
   end
